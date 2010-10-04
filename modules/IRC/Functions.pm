14e6ae7bd98c7134554e4e8116a08f47e08fab73
Russell Bradford <russell@surgehost.net>
2010-10-04 01:08:21 +0100

    Added NOTICE, MODE, KICK and QUIT functions that the bot will no doubt use sometime in the future

--- modules/IRC/Functions.pm
+++ modules/IRC/Functions.pm
@@ -11,6 +11,34 @@ sub MSG {
 	&pBot::send_sock("PRIVMSG ".$chan." :".$msg."\n");
 }
 
+sub NOTICE {
+	my($chan,$msg) = @_;
+ 	chomp($chan);
+ 	chomp($msg);
+	&pBot::send_sock("NOTICE ".$chan." :".$msg."\n");
+}
+
+sub MODE {
+	my($chan,$modes) = @_;
+ 	chomp($chan);
+ 	chomp($modes);
+	&pBot::send_sock("MODE ".$chan." :".$modes."\n");
+}
+
+sub QUIT {
+	my($msg) = @_;
+ 	chomp($msg);
+	&pBot::send_sock("QUIT :".$msg."\n");
+}
+
+sub KICK {
+	my($chan,$nick,$reason) = @_;
+ 	chomp($chan);
+ 	chomp($nick);
+ 	chomp($reason);
+	&pBot::send_sock("KICK ".$chan." ".$nick." :".$reason."\n");
+}
+
 sub NICK {
 	my($nick) = @_;
  	chomp($nick);
